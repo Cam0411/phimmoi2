31a7dcb89e184609fdd2eaf3f0b95041a7bd2c4c
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2021-12-25 11:08:15 +0700

    Add files via upload

--- main/app.js
+++ main/app.js
@@ -9,3 +9,42 @@ function scrollValue() {
 }
 
 window.addEventListener('scroll', scrollValue);
+
+// modal 
+
+var menu = document.querySelector(".menu")
+var modal = document.querySelector(".modal")
+var modalContent = document.querySelector(".modal__content")
+var modalbackground = document.querySelector(".modal__background")
+menu.addEventListener("click",function(){
+    modal.classList.add("open")
+})
+modal.addEventListener("click",function(){
+    modal.classList.remove("open")
+    modalbackground.classList.remove("open2")
+})
+modalContent.addEventListener("click",function(event){
+    event.stopPropagation()
+})
+menu.addEventListener("click",function(){
+    modalbackground.classList.toggle("open2")
+})
+
+
+var search = document.querySelector(".search")
+var searchModal = document.querySelector(".modal__search ")
+var  modalbackground = document.querySelector(".modal__background")
+var searchContent = document.querySelector(".search__content ")
+search.addEventListener("click",function(){
+    searchModal.classList.add("open")
+})
+searchModal.addEventListener("click",function(){
+    searchModal.classList.remove("open")
+    modalbackground.classList.remove("open2")
+})
+searchContent.addEventListener("click",function(event){
+    event.stopPropagation()
+})
+search.addEventListener("click",function(){
+    modalbackground.classList.toggle("open2")
+})
